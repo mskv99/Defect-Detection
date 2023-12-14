ba3bdf6a915c200acfcb05147f04e72ee43e358f
Andrei Moskovtsev <moskovtsev.af@phystech.edu>
2023-12-14 14:58:38 +0300

    Update config.py
    changed classes in config.py

--- NetDirectory/config.py
+++ NetDirectory/config.py
@@ -1,20 +1,24 @@
 import torch
 
-BATCH_SIZE = 4 # Increase / decrease according to GPU memeory.
+
+BATCH_SIZE = 8 # Increase / decrease according to GPU memeory.
 RESIZE_TO = 640 # Resize the image for training and transforms.
 NUM_EPOCHS = 40 # Number of epochs to train for.
-NUM_WORKERS = 4 # Number of parallel workers for data loading.
-
+NUM_WORKERS = 2 # Number of parallel workers for data loading.
+LEARNING_RATE = 0.007475
+STEP = 10
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 # Training images and XML files directory.
-TRAIN_DIR = 'data/BCCD.v3-raw.voc/train'
+TRAIN_DIR = '/content/Defect-Detection/NetDirectory/custom_data/train'
 # Validation images and XML files directory.
-VALID_DIR = 'data/BCCD.v3-raw.voc/valid'
+VALID_DIR = '/content/Defect-Detection/NetDirectory/custom_data/valid'
+TEST_DIR = '/content/Defect-Detection/NetDirectory/custom_data/test'
 
-# Classes: 0 index is reserved for background.
+
+
 CLASSES = [
-    '__background__', 'RBC', 'WBC', 'Platelets'
+    '__background__', 'bridge', 'gap', 'sraf'
 ]
 
 NUM_CLASSES = len(CLASSES)
@@ -23,4 +27,4 @@ NUM_CLASSES = len(CLASSES)
 VISUALIZE_TRANSFORMED_IMAGES = False
 
 # Location to save model and plots.
-OUT_DIR = 'outputs'
+OUT_DIR = '/content/Defect-Detection/NetDirectory/outputs'
