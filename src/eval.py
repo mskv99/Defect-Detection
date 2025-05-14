f4605ccb80b765a779dd7682ec65c4496e3ae892
Andrei Moskovtsev <moskovtsev.af@phystech.edu>
2025-05-14 18:07:02 +0000

    added confidence and iou thresholds, batch size and split parameters to evaluation script

--- src/eval.py
+++ src/eval.py
@@ -2,9 +2,13 @@ from ultralytics import YOLO
 import fire
 import torch
 
-def main(data_config: str = 'data/processed/defects/data.yaml',
+def main(data_config: str = 'data/processed/dataset-v1/data.yaml',
          weights: str = None,
-         project: str = '/mnt/data/amoskovtsev/defect-detection/'):
+         project: str = '/mnt/data/amoskovtsev/defect-detection/',
+         conf: float = 0.001,
+         iou: float = 0.6,
+         split: str = 'val',
+         batch: int = 16):
   if torch.cuda.is_available():
     device = 'cuda:0'
   elif torch.backends.mps.is_available():
@@ -16,7 +20,11 @@ def main(data_config: str = 'data/processed/defects/data.yaml',
   model = YOLO(weights)
   metrics = model.val(data=data_config,
                       project = project,
-                      device = device)
+                      device = device,
+                      conf = conf,
+                      iou = iou,
+                      split = split,
+                      batch = batch)
 
 if __name__=='__main__':
   fire.Fire(main)
